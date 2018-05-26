8cf0a72ad8f9d825753fea85ba17eb9b187b7d74
Knitschi <knitschi@freenet.de>
2018-05-26 13:29:44 +0200

    Fix a bug in cpfReadCurrentCacheVariables() that occured when the first cache variable had empty values.

--- Functions/cpfProjectUtilities.cmake
+++ Functions/cpfProjectUtilities.cmake
@@ -971,6 +971,7 @@ endfunction()
 function( cpfReadCurrentCacheVariables variableNamesOut variableValuesOut variableTypesOut variableDescriptionsOut )
 
 	get_cmake_property( cacheVariables CACHE_VARIABLES)
+	rotateCacheVariableWithAllValuesToTheFront(cacheVariables "${cacheVariables}") # We have to do this to make sure we later do net append empty elements to empty lists.
 	set(cacheValues)
 	set(cacheTypes)
 	set(cacheDescriptions)
@@ -978,7 +979,8 @@ function( cpfReadCurrentCacheVariables variableNamesOut variableValuesOut variab
 	foreach(variable ${cacheVariables})
 
 		# values
-		set(value ${${variable}})
+		cpfGetCacheVariableValues( value type helpString ${variable})
+		
 		cpfListLength(valueLength "${value}") 
 		if( ${valueLength} GREATER 1) 
 			# for lists one escape level is needed to get a list of lists
@@ -988,12 +990,9 @@ function( cpfReadCurrentCacheVariables variableNamesOut variableValuesOut variab
 		else()
 			list(APPEND cacheValues "${value}")
 		endif()
-		# type		
-		get_property( type CACHE ${variable} PROPERTY TYPE )
 		list(APPEND cacheTypes ${type})
-		# description
-		get_property( helpString CACHE ${variable} PROPERTY HELPSTRING )
 		list(APPEND cacheDescriptions "${helpString}")
+
 	endforeach()
 
 	# assert that all lists are of the same length
@@ -1013,6 +1012,38 @@ function( cpfReadCurrentCacheVariables variableNamesOut variableValuesOut variab
 	set( ${variableValuesOut} "${cacheValues}" PARENT_SCOPE)
 	set( ${variableTypesOut} "${cacheTypes}" PARENT_SCOPE)
 	set( ${variableDescriptionsOut} "${cacheDescriptions}" PARENT_SCOPE)
+
+endfunction()
+
+#---------------------------------------------------------------------------------------------
+# The function goes through the list of cache variables and finds the first that has a value,
+# a type and a helpsting. It puts this variable at the top of the returned list.
+function( rotateCacheVariableWithAllValuesToTheFront variablesOut variables)
+
+	set(index 0)
+	foreach(variable ${variables})
+		cpfGetCacheVariableValues( value type helpstring ${variable})
+		if(value AND type AND helpstring)
+			# exchange variables
+			list(REMOVE_AT variables ${index})
+			list(INSERT variables 0 ${variable})
+			set(${variablesOut} "${variables}" PARENT_SCOPE)
+			return()
+		endif()
+		cpfIncrement(index)
+	endforeach()
+	message(FATAL_ERROR "There was no cache variable that has all values defined.")
+
+endfunction()
+
+#---------------------------------------------------------------------------------------------
+function( cpfGetCacheVariableValues valueOut typeOut helpstringOut variable)
+
+	set(${valueOut} ${${variable}} PARENT_SCOPE)
+	get_property( type CACHE ${variable} PROPERTY TYPE )
+	set(${typeOut} ${type} PARENT_SCOPE)
+	get_property( helpString CACHE ${variable} PROPERTY HELPSTRING )
+	set(${helpstringOut} ${helpString} PARENT_SCOPE)
 
 endfunction()
 
