da6c9ba8aa3eb0b4119e13616c6a4a7e3aa6ae16
Knitschi <11889116+Knitschi@users.noreply.github.com>
2019-03-10 15:05:24 +0100

    removes debug output.

--- Scripts/prepareCIRepoForBuild.cmake
+++ Scripts/prepareCIRepoForBuild.cmake
@@ -171,7 +171,6 @@ else()
                 # Commit the changes made to the packges.
                 foreach(package ${ownedLoosePackages})
                     cpfGetAbsPackageDirectory( packageDir ${package} ${ROOT_DIR})
-                    devMessage("${packageDir}")
                     cpfWorkingDirectoryIsDirty(isDirty "${packageDir}")
                     if(isDirty)
                         cpfExecuteProcess( unused "git commit . -m\"clang-format\"" "${packageDir}")
