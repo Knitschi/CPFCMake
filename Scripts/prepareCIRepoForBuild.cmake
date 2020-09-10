c1dc746cde0584a5bfcd31e5bc73d11c8153ffa6
Knitschi <knitschi@freenet.de>
2020-09-10 20:22:25 +0200

    Reverts the last commit.

--- Scripts/prepareCIRepoForBuild.cmake
+++ Scripts/prepareCIRepoForBuild.cmake
@@ -167,7 +167,7 @@ else()
                 # As long as we only do this on Linux we can use the python3 command directly.
                 # I failed to do this using the FindPython3 module, because it does not work in script mode.
                 message(STATUS "Run clang-format")
-                cpfExecuteProcess( unused "python3 Sources/CPFBuildscripts/0_GetDependencies.py" ${ROOT_DIR})
+                cpfExecuteProcess( unused "python3 Sources/CPFBuildscripts/0_CopyScripts.py" ${ROOT_DIR})
                 cpfExecuteProcess( unused "conan install -pr \"${ROOT_DIR}/Sources/CIBuildConfigurations/ConanProfile-${CONFIG}\" -if \"${ROOT_DIR}/Configuration/${CONFIG}\" Sources --build=missing" ${ROOT_DIR})
                 cpfExecuteProcess( unused "python3 3_Make.py ${CONFIG} --target clang-format" ${ROOT_DIR})
 
