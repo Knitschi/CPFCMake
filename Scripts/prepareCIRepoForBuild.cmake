d75ad68d1dc453f0e0b70febcc0842be8a169c89
Knitschi <knitschi.public@freenet.de>
2022-01-15 20:22:31 +0100

    Fixes the superfluous script execution when  using consn

--- Scripts/prepareCIRepoForBuild.cmake
+++ Scripts/prepareCIRepoForBuild.cmake
@@ -173,11 +173,15 @@ else()
                 # As long as we only do this on Linux we can use the python3 command directly.
                 # I failed to do this using the FindPython3 module, because it does not work in script mode.
                 message(STATUS "Run clang-format")
-                cpfExecuteProcess( unused "python3 \"${CPFBuildscripts_DIR}/0_CopyScripts.py\" --CPFCMake_DIR \"${CPFCMake_DIR}\" --CIBuildConfigurations_DIR \"${CIBuildConfigurations_DIR}\"" "${ROOT_DIR}")
+
                 # Get dependency requirements.
                 if("${PACKAGE_MANAGER}" STREQUAL "conan")
+                    message(STATUS "Run conan install")
                     cpfExecuteProcess( unused "conan install . -pr \"${CONFIG}\" -if \"Configuration/${CONFIG}\" --build=missing" ${ROOT_DIR})
+                else()
+                    cpfExecuteProcess( unused "python3 \"${CPFBuildscripts_DIR}/0_CopyScripts.py\" --CPFCMake_DIR \"${CPFCMake_DIR}\" --CIBuildConfigurations_DIR \"${CIBuildConfigurations_DIR}\"" "${ROOT_DIR}")
                 endif()
+
                 # Format the sources by building the clang-format target.
                 cpfExecuteProcess( unused "python3 4_Make.py ${CONFIG} --target clang-format" "${ROOT_DIR}")
 
